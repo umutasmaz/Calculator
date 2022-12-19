3331e9bde714db3e6c10bcd7ddf15d0640ccd08a
umutasmaz <76012751+umutasmaz@users.noreply.github.com>
2022-12-19 19:17:48 +0300

    Add files via upload

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Umut Asmaz on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sumClicked(_ sender: Any) {
+        if let firstNumber = Int (firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber + secondNumber
+                resultLabel.text=String(result)
+            }
+        }
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func multipyClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+            
+        }
+        
+    }
+    
+}
